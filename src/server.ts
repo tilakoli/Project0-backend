450778a7785ff1b202daefb405dd983ddc16030c
tilakoli <tilak.81oli@gmail.com>
2025-12-06 11:44:24 +0545

    create db config and basic files and folders

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import { connectDB } from './config/database';
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ app.use(express.json());
 app.get('/health', (req, res) => {
   res.json({ status: 'ok', message: 'Server is running' });
 });
+
+// Connect to MongoDB
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
